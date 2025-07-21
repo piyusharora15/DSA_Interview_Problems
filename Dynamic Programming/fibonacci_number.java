5c641a5d565e49606583ba31c49c7b5354ff08fa
Piyush Arora <piyusharora1504@gmail.com>
2025-07-21 15:13:18 +0530

    All approaches code and complexity analysis.

--- Dynamic Programming/fibonacci_number.java
+++ Dynamic Programming/fibonacci_number.java
@@ -1,13 +1,13 @@
 /*
     Company Tags : Amazon, Microsoft, OYO Rooms, Snapdeal, MakeMyTrip, Goldman Sachs, MAQ Software, Adobe
-    Leetcode Lin : https://leetcode.com/problems/fibonacci-number/
+    Leetcode Link : https://leetcode.com/problems/fibonacci-number/
 */
 
 // Approach 1 -> Using Recursion:
 /*
 Time Complexity: O(2ⁿ) ; because it solves the same subproblems again and again — exponential time.
 Space Complexity: O(n) ; due to recursion call stack depth.
-*/
+*/  class Solution{
     public int fib(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
@@ -15,11 +15,13 @@ Space Complexity: O(n) ; due to recursion call stack depth.
         return n;
         return dp[n] = fib(n-1) + fib(n-2);
     }
+}
 // Approach 2 -> Using Memoization:
 /* 
 Time Complexity: O(n) ; each subproblem is solved only once and stored.
 Space Complexity: O(n) ; for recursion stack + dp[] array. 
 */
+    class Solution{
     public int fib(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
@@ -32,12 +34,13 @@ Space Complexity: O(n) ; for recursion stack + dp[] array.
         }
         return dp[n];
     }
-
+    }
 // Approach 3 -> Using Tabulation:
 /*
 Time Complexity: O(n) ; loop runs once from 2 to n.
 Space Complexity: O(n) ; for the dp[] array.
 */
+    class Solution{
     public int fib(int n) {
         int prev2 = 0;
         int prev = 1;
@@ -51,12 +54,13 @@ Space Complexity: O(n) ; for the dp[] array.
         }
         return prev;
     }
-
+    }
 // Approach 4 -> Using Space Optimization:
 /*
 Time Complexity: O(n) ; we are simply running an iterative loop.
 Space Complexity: O(1) ; as we are not using any extra space.
 */
+    class Solution{
     public int fib(int n) {
         int prev2 = 0;
         int prev = 1;
@@ -69,4 +73,5 @@ Space Complexity: O(1) ; as we are not using any extra space.
             prev = cur_i;
         }
         return prev;
+    }
     }
